ebf1911645af8fd04a45a75609ae64a63d566030
Haneen Abdel-Wahab <han.abdelwahab01@gmail.com>
2026-02-05 14:11:52 +0200

    Add new icons

---
+++ src/components/icons/StarIcon.tsx
@@ -0,0 +1,15 @@
+export default function StarIcon({ className = "" }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 16 15"
+      className={className}
+      fill="none"
+    >
+      <path
+        d="M7.97907 -2.26498e-05L9.38117 5.64067L15.1791 6.05027L10.2477 9.12681L11.6498 14.7675L7.19998 11.0282L2.26863 14.1048L4.44983 8.71722L-1.13754e-05 4.97793L5.79788 5.38753L7.97907 -2.26498e-05Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
